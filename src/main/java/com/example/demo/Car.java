a8624def115829dc30c82df6655574ed24fe7f5c
Jakub Marcinkowski <s21021@pjwstk.edu.pl>
2021-10-16 10:19:35 +0200

    car

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Car {
+    private String Model;
+    private float Enginer;
+    private String kolor;
+
+    public Car(String model, float enginer, String kolor) {
+        Model = model;
+        Enginer = enginer;
+        this.kolor = kolor;
+    }
+}
